2b87b3143a3d0c91bbb6256ff8b8a3ae05a32bfa
Steve Tran <joonkins@protonmail.com>
2022-05-05 16:02:36 -0400

    reversed

--- practice-for-week-04-tdd-style-long-practice-main/problems/reverse-string.js
+++ practice-for-week-04-tdd-style-long-practice-main/problems/reverse-string.js
@@ -1,3 +1,25 @@
-module.exports = function reverseString(string) {
+module.exports = function reverseString(string){
+ 
   // Your code here
+
+  let word = "";
+
+  for(let i = string.length-1; i >= 0; i--) {
+    let letter = string[i];
+
+    word+=letter
+  }
+
+  // console.log(string);
+
+return word
+
+
 };
+
+// console.log(reverseString("fun"));
+
+
+
+
+
--- practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
+++ practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
@@ -1,1 +1,10 @@
 // Your code here
+const {expect} = require("chai"); 
+const reverseString = require("../problems/reverse-String")
+
+describe ("reverseString" , function () {
+    it("should reverse input string", function(){
+        let test = reverseString
+        expect(test("fun")).to.equal("nuf")
+    })
+})
